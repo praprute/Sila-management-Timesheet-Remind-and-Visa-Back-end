5babf12693667c094f880567ca3ca02d474f019e
junior <trader.junior492047@gmail.com>
2020-11-08 04:03:29 +0700

    config

--- confix.js
+++ confix.js
@@ -1,9 +1,9 @@
 module.exports = {
     secret: '4rlskrgsglwsdhgksjfgsjlhwirlgishlfhlfhlrthleirlgl5y983094702424qrhgjg34jg-340jgr-u478o',
     dbOption: {
-        host: '127.0.0.1',
+        host: '128.199.179.127',
         user: 'root',
-        password: '12345678',
+        password: 'silaapp4920',
         port: 3306,
         database: 'sila-management',
         dateStrings: true
@@ -11,3 +11,4 @@ module.exports = {
     ACCESS_KEY: 'KGWERP32XXOI7XLIXW7H',
     SECRET_KEY: 'Iu8IaJEMIlSvcJBzSv7XzlCtU8VRvHZiygZUW1iIZqM'
 }
+///Users/juniorchokies/dumps/Dump20201108
